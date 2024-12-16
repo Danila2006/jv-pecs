6c8497ae784015a4fb510df621f87285a2676ea4
Danil Tarasiuk <vodafonenous9388@gmail.com>
2024-12-16 14:49:14 +0100

    Modified BulldozerProducer.java, ExcavatorProducer.java, MachinesProducer.java, MachinesServiceImpl.java, TruckProducer.java after AI checking

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -25,7 +25,7 @@ public class MachineServiceImpl implements MachineService {
     }
 
     @Override
-    public void fill(List machines, Object value) {
+    public void fill(List machines, Machine value) {
         for (int i = 0; i < machines.size(); i++) {
             machines.set(i, value);
         }
